54c92b5b828e94c04a052cbce5c312985cf4dc79
e1ha <59583520+e1ha@users.noreply.github.com>
2021-02-13 20:02:33 -0800

    Update

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,115 @@
 
 class ProductItem extends HTMLElement {
   // TODO
-}
+  constructor() {
+    super();
+    //product-list is the shadow host
+    //var productList = document.getElementById("product-list");
 
+    var shadow = this.attachShadow({mode: 'open'});
+    shadow.innerHTML = `
+      <style> 
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+      </style>
+
+      <li class="product">
+        <img id="image" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+        <p id="title" class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+        <p id="price" class="price">$109.95</p>
+        <button id="buttons" onclick="alert('Added to Cart!'); incrementCart();">Add to Cart</button>
+      </li>
+    `;
+
+    
+  
+  }
+  
+}
+var retrievedObject = localStorage.getItem('arrayInLS');
 customElements.define('product-item', ProductItem);
+
+var storage = window.localStorage; 
+
+var cartBtn = shadowRoot.getElementById("buttons"); 
+    var cartCount = document.getElementById("cart-count");
+
+    /*function incrementCart() {
+      if (cartBtn.textContent == "Add to Cart") {
+        cartBtn.textContent = "Remove from Cart"; 
+        var currentCounts = cartCount.textContent; 
+        cartCount.textContent = parseInt(currentCounts,10) + 1; 
+        alert("hello"); 
+        //storage.setItem('inCart', JSON.parse(retrievedObject)[i].id); 
+      }
+      else if (cartBtn.textContent == "Remove from Cart") {
+        var currentCounts = cartCount.textContent; 
+        cartCount.textContent = parseInt(currentCounts,10) - 1; 
+        cartBtn.textContent = "Add to Cart"; 
+        alert("hi"); 
+        //remove item id from local storage
+      }
+  
+
+  }*/
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,55 @@
 // Script.js
 
+var storage = window.localStorage;
+
+//how to check before your initial fetch request to see if something is first in local storage
+//alert(storage.getItem('user'));
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(array => storage.setItem('arrayInLS', JSON.stringify(array))); //downloads content of the url
 });
+
+var productList = document.getElementById("product-list");
+var i = 1;
+for (i = 0; i < 21; i++) {
+  var product = document.createElement('product-item'); 
+  var image = product.shadowRoot.getElementById('image'); 
+  image.setAttribute('src', JSON.parse(retrievedObject)[i].image);
+  var title = product.shadowRoot.getElementById('title');
+  title.textContent = JSON.parse(retrievedObject)[i].title;
+  var price = product.shadowRoot.getElementById('price');
+  price.textContent = "$"+JSON.parse(retrievedObject)[i].price;
+
+  productList.appendChild(product); 
+}
+
+/*var addToCartButton = shadowRoot.getElementById('buttons');
+addToCartButton.addEventListener("click", incrementCart); 
+var cartCount = document.getElementById("cart-count"); */
+
+/*function incrementCart() {
+  alert("hello");
+  var currentCounts = cartCount.textContent; 
+  cartCount.textContent = parseInt(currentCounts,10) + 1; 
+}*/
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem('arrayInLS');
+//console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+/*  fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  //returns the promise from the fetch
+  let response = await fetch(url);
+
+  //if the promise is successful
+  if (response.ok) { // if HTTP-status is 200-299
+  // get the response body (the method explained below)
+  let json = await response.json();
+  } else {
+  alert("HTTP-Error: " + response.status);
+  }*/
